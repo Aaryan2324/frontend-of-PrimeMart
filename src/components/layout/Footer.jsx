e6791cc588043ece2c32c0d896ca18b49ffcfc9a
Aaryan2324 <129336503+Aaryan2324@users.noreply.github.com>
2024-05-29 06:37:17 +0530

    This is the frontend of the application

---
+++ src/components/layout/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer class="py-1 pt-5">
+      <p class="text-center mt-1 fw-bold">
+        Prime Mart - 2024-2026, All Rights Reserved
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
